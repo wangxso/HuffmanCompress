5d3b0e6da399f40005f0eb27b6ca91bf43efb064
wangxinsheng <wangxinsheng0102@163.com>
2018-12-26 14:52:06 +0800

    拆分

---
+++ cmake-build-debug/CMakeFiles/yasuo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yasuo.dir/main.cpp.o"
+  "yasuo.pdb"
+  "yasuo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yasuo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
